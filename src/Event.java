48023f3a0422fbc4ec04fbe8d5d6180b99632dc9
Jacob Gaumond <jacobgaumond@hotmail.com>
2026-02-19 23:00:31 -0500

    Renames Event class to SimulationEvent to prevent name collisions with the Java-provided class

--- src/Event.java
+++ src/Event.java
@@ -1,14 +1,14 @@
-public abstract class Event {
+public abstract class SimulationEvent {
     protected static final String DELIMITER = "~";
 
     // Concrete subclasses are expected to provide a constructor that only takes a String object. This String should
     // match the format of those outputted by its serialize() method, such that serialize() can serialize the object
-    // (i.e., Event -> String) and the String constructor can deserialize the String (i.e., String -> Event).
+    // (i.e., SimulationEvent -> String) and the String constructor can deserialize the String (i.e., String -> SimulationEvent).
 
     /**
      * Serializes the object into a string, separated by the DELIMITER.
      *
-     * @return String representation of the Event object.
+     * @return String representation of the SimulationEvent object.
      */
     public abstract String serialize();
 }
