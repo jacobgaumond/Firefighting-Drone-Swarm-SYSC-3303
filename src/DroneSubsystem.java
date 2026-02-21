806c6d7403d4fba4c8adf6e30724b363a08fedfa
olivezy <oliveburgoin@gmail.com>
2026-02-21 01:54:27 -0500

    DroneSubsystem minor fixes and cleanup

--- src/DroneSubsystem.java
+++ src/DroneSubsystem.java
@@ -27,54 +27,49 @@ public class DroneSubsystem implements Runnable {
 //        }
 //    }
 
+    private static final int NOZZLE_OPEN_DELAY_MS = 100;
+    private static final int NOZZLE_CLOSE_DELAY_MS = 100;
+
+    private static final double FLUID_RATE_ML_MS= 0.25;
+
     private MessageBox incomingMessageBox;
     private MessageBox schedulerMessageBox;
 
     private static int nextIdValue = 1;//self ID creation
 
-    private int drone_ID;
-    private int x_coord;
-    private int y_coord;
+    private int droneId;
+    private int coordX;
+    private int coordY;
 
-    private int x_targetcoords;
-    private int y_targetcoords;
+    private int targetCoordX;
+    private int targetCoordY;
     private int batteryTravelDistance;
     private int fluidAmount;
 
     private int fluidAmountToDrop;
 
-    private static double fluidrate =0.25;
-
     private final DroneStateMachine droneSM;
-
-
 
     // WITHOUT MESSAGE BOXING
     public DroneSubsystem() {
-        this.droneSM = new DroneStateMachine();
-        this.drone_ID = nextIdValue++;
-        this.x_coord =0;
-        this.y_coord = 0;
-        this.fluidAmount = 15;
-        this.x_targetcoords=0;
-        this.y_targetcoords=0;
-        this.batteryTravelDistance =1000;  // TravelDistanceLevel to be decided
-
+        this(null, null);
     }
 
     //WITH MESSAGEBOX
     public DroneSubsystem(MessageBox incomingMessageBox, MessageBox schedulerMessageBox) {
         this.schedulerMessageBox = schedulerMessageBox;
         this.incomingMessageBox = incomingMessageBox;
+
+        //Core initlization
         this.droneSM = new DroneStateMachine();
-        this.drone_ID = nextIdValue++;
-        this.x_coord = 0;
-        this.y_coord = 0;
+        this.droneId = nextIdValue++;
+
+        //Default starting values
+        this.coordX = 0;
+        this.coordY = 0;
         this.fluidAmount = 15;
         this.batteryTravelDistance = 1000;  // TravelDistanceLevel change eventually
     }
-
-
 
     @Override
     public void run() {
@@ -82,7 +77,7 @@ public class DroneSubsystem implements Runnable {
         Message registerMessage = new Message(
                 "Scheduler",
                 "DroneSubsystem",
-                String.valueOf(drone_ID),
+                String.valueOf(droneId),
                 Message.MessageType.DroneRegistration
         );
         schedulerMessageBox.putMessage(registerMessage);
@@ -92,45 +87,43 @@ public class DroneSubsystem implements Runnable {
             Message message = incomingMessageBox.getMessage();
             if (message == null) {
                 boxOpen = false;
-            }  else {
-                    handleMessage(message);
+            } else {
+                handleMessage(message);
             }
 
         } while (boxOpen);
     }
 
 
-    public void handleMessage(Message message){
-        if(message.getMessageType() == Message.MessageType.DroneRequest) {
+    public void handleMessage(Message message) {
+        if (message.getMessageType() == Message.MessageType.DroneRequest) {
             DroneRequest droneEvent = new DroneRequest(message.getMessageData());
             System.out.println("[DroneSubsystem] Received DroneEvent: " + droneEvent);
 
-            if(droneEvent.getDroneEvent()==DroneEvent.FIRE_ASSIGNED||droneEvent.getDroneEvent()==DroneEvent.RETURN_BASE_REQUEST) {
-                this.x_targetcoords = droneEvent.getTargetX();
-                this.y_targetcoords = droneEvent.getTargetY();
+            if (droneEvent.getDroneEvent() == DroneEvent.FIRE_ASSIGNED || droneEvent.getDroneEvent() == DroneEvent.RETURN_BASE_REQUEST) {
+                this.targetCoordX = droneEvent.getTargetX();
+                this.targetCoordY = droneEvent.getTargetY();
             }
-            if(droneEvent.getDroneEvent()==DroneEvent.FIRE_ASSIGNED) {
+            if (droneEvent.getDroneEvent() == DroneEvent.FIRE_ASSIGNED) {
                 this.fluidAmountToDrop = droneEvent.getAmountToDrop();
             }
             //sends the message to the statemachine
-            handleEvent(droneEvent.getDroneEvent(),droneEvent.serialize());
+            droneSM.handleEvent(droneEvent.getDroneEvent(), droneEvent.serialize(), this);
 
-            //sends the new status back to the Scheduler
-            schedulerMessageBox.putMessage(sendStatus());
-        } else if(message.getMessageType() == Message.MessageType.DroneResponse) {
-            //handle other types
-            System.out.println("[DroneSubsystem] Received response: " + message.getMessageData());
+            //updates the Scheduler
+            updateScheduler();
+
         } else {
             System.out.println("[DroneSubsystem] Received unknown message type: " + message.getMessageType());
         }
 
     }
 
-    public Message sendStatus(){ // int ID: statemachine drone: int x: int y: int fluidAmount: int battery
+    public Message sendStatus() { //creates the message
         DroneResponse status = new DroneResponse(
-                drone_ID,
+                droneId,
                 droneSM.getCurrentState().toString(),
-                x_coord, y_coord,
+                coordX, coordY,
                 fluidAmount,
                 batteryTravelDistance,
                 fluidAmountToDrop
@@ -139,111 +132,130 @@ public class DroneSubsystem implements Runnable {
         return new Message("Scheduler", "DroneSubsystem", status.serialize(), Message.MessageType.DroneResponse);
     }
 
-    private void sendAcknowledgement(){
-        System.out.println("[DroneSubsystem] Sending DroneSubsystem to Scheduler");
+    public void updateScheduler() { //updates scheduling logic
+        System.out.println("[Drone " + droneId + "] Notifying scheduler of status.");
         schedulerMessageBox.putMessage(sendStatus());
     }
 
-    public void handleEvent(DroneEvent event, String payload) {
-        droneSM.handleEvent(event, payload,this);
-    }
-
-    // Drone Movement functions
+    //** Drone Movement & Modification Functions **//
     public void flyToFire(String payload) {
         //batteryTravelDistance -= calculateBatteryUsage();
-        System.out.println("[Drone " + drone_ID + "] Flying to fire: " + payload);
+        System.out.println("[Drone " + droneId + "] Flying to fire: " + payload);
         //just for this iteration
-        x_coord=x_targetcoords;
-        y_coord=y_targetcoords;
+        coordX = targetCoordX;
+        coordY = targetCoordY;
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        droneSM.handleEvent(DroneEvent.ARRIVAL,payload,this);
+        droneSM.handleEvent(DroneEvent.ARRIVAL, payload, this);
     }
 
     public void returnToBase(String payload) {
         //batteryTravelDistance -= calculateBatteryUsage();
-       //for this iteration
+        //for this iteration
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        x_coord=0;
-        y_coord=0;
-        droneSM.handleEvent(DroneEvent.ARRIVAL,payload,this);
-        System.out.println("[Drone " + drone_ID + "] Returning to base.");
+        coordX = 0;
+        coordY = 0;
+        droneSM.handleEvent(DroneEvent.ARRIVAL, payload, this);
+        System.out.println("[Drone " + droneId + "] Returning to base.");
     }
 
     public void openNozzle() {
-        System.out.println("[Drone " + drone_ID + "] Nozzle opened, dropping agent.");
+        System.out.println("[Drone " + droneId + "] Nozzle opened, dropping agent.");
+        long dropTime = (long) (fluidAmountToDrop / FLUID_RATE_ML_MS)+ NOZZLE_OPEN_DELAY_MS;
         try {
-            Thread.sleep(100);
+            Thread.sleep(dropTime);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         fluidAmount -= fluidAmountToDrop;
-        System.out.println("[Drone " + drone_ID + "] extinguishing fire.");
+        System.out.println("[Drone " + droneId + "] extinguishing fire.");
         droneSM.handleEvent(DroneEvent.FIRE_EXTINGUISHED, "", this);
     }
 
     public void closeNozzle(String payload) {
-        System.out.println("[Drone " + drone_ID + "] Nozzle closed.");
+        try {
+            Thread.sleep(NOZZLE_CLOSE_DELAY_MS); //Nozzle Door Close Time
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("[Drone " + droneId + "] Nozzle closed.");
     }
 
     public void restore() {//restores battery and restores fuel level
-        System.out.println("[Drone " + drone_ID + "] restocking drone.");
+        System.out.println("[Drone " + droneId + "] restocking drone.");
         this.batteryTravelDistance = 1000;
         this.fluidAmount = 15;
     }
 
-
-    public void updateScheduler() { //updates scheduling logic
-        System.out.println("[Drone " + drone_ID + "] Notifying scheduler of status.");
-        schedulerMessageBox.putMessage(sendStatus());
-    }
-
+    //This is to be decided later with different information//
     public void handleFault() {
         // TODO: log fault, notify scheduler, await repair event
-        System.out.println("[Drone " + drone_ID + "] FAULTED. Awaiting repair.");
+        System.out.println("[Drone " + droneId + "] FAULTED. Awaiting repair.");
         schedulerMessageBox.putMessage(sendStatus());
     }
 
 
-    // Specific checks to be done
-
+    // DRONE CHECKING//
     public boolean hasBattery() {
-        double droneToFire = Math.sqrt(Math.pow(x_targetcoords - x_coord, 2) + Math.pow(y_targetcoords - y_coord, 2));
-        double fireToBase = Math.sqrt(Math.pow(x_targetcoords, 2) + Math.pow(y_targetcoords, 2));
+        double droneToFire = Math.sqrt(Math.pow(targetCoordX - coordX, 2) + Math.pow(targetCoordY - coordY, 2));
+        double fireToBase = Math.sqrt(Math.pow(targetCoordX, 2) + Math.pow(targetCoordY, 2));
         double totalDistance = droneToFire + fireToBase;
         return batteryTravelDistance >= totalDistance;
     }
 
     public int calculateBatteryUsage() {
-        double droneToFire = Math.sqrt(Math.pow(x_targetcoords - x_coord, 2) + Math.pow(y_targetcoords - y_coord, 2));
-        double fireToBase = Math.sqrt(Math.pow(x_targetcoords, 2) + Math.pow(y_targetcoords, 2));
+        double droneToFire = Math.sqrt(Math.pow(targetCoordX - coordX, 2) + Math.pow(targetCoordY - coordY, 2));
+        double fireToBase = Math.sqrt(Math.pow(targetCoordX, 2) + Math.pow(targetCoordY, 2));
         return (int) Math.ceil(droneToFire + fireToBase);
     }
 
     public boolean hasAgent() {
         return fluidAmount > 0;
     }
-    public int getDrone_ID() {return drone_ID;}
 
-    public int getX_coord() { return x_coord; }
-    public void setX_coord(int x_coord) { this.x_coord = x_coord; }
+    //** GETTERS AND SETTERS **//
+    public int getDroneId() {
+        return droneId;
+    }
 
-    public int getY_coord() { return y_coord; }
-    public void setY_coord(int y_coord) { this.y_coord = y_coord; }
+    public int getCoordX() {
+        return coordX;
+    }
 
-    public int getFluidAmount() { return fluidAmount; }
-    public void setFluidAmount(int fluidAmount) { this.fluidAmount = fluidAmount; }
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
 
-    public void setCurrentState(DroneState state){droneSM.setCurrentState(state);}
+    public int getCoordY() {
+        return coordY;
+    }
 
-    public DroneState getCurrentState(){return droneSM.getCurrentState();}
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
+
+    public int getFluidAmount() {
+        return fluidAmount;
+    }
+
+    public void setFluidAmount(int fluidAmount) {
+        this.fluidAmount = fluidAmount;
+    }
+
+    public void setCurrentState(DroneState state) {
+        droneSM.setCurrentState(state);
+    }
+
+    public DroneState getCurrentState() {
+        return droneSM.getCurrentState();
+    }
 
     // Static method to check how many drones have been created
     public static int getTotalDronesCreated() {
