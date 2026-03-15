592ed51916e7c46f4c5645332a6595cc0b3b0dc9
Jacob Gaumond <jacobgaumond@hotmail.com>
2026-03-15 04:57:23 -0400

    Changes Message to support serialization/deserialization (to support sending messages over UDP in a later commit)

--- src/Message.java
+++ src/Message.java
@@ -12,12 +12,50 @@ public class Message {
     private final String        destinationName;
     private final String        sourceName;
 
+    private final static String DELIMITER = "#";
+
     public Message(String destinationName, String sourceName, String messageData, MessageType messageType) {
         type = messageType;
         data = messageData;
 
         this.destinationName = destinationName;
         this.sourceName      = sourceName;
+    }
+
+    /**
+     * Constructor that creates a Message object from the output of the Message class's serialize() method.
+     * In other words, it deserializes Strings to create Message objects.
+     *
+     * @param serializedMessage A String containing a serialized Message object.
+     */
+    public Message(String serializedMessage) {
+        String[] objectVariables = serializedMessage.split(DELIMITER);
+
+        // Convert "type" (stored in objectVariables[0]) from String to MessageType
+        MessageType type = null;
+        try {
+            type = MessageType.valueOf(objectVariables[0]);
+        }
+        catch (IllegalArgumentException iae) {
+            iae.printStackTrace();
+            System.exit(1);
+        }
+        // Initialize object variables
+        this.type       = type;
+        data            = objectVariables[1];
+        destinationName = objectVariables[2];
+        sourceName      = objectVariables[3];
+    }
+
+    /**
+     * Serializes the Message object into a String.
+     *
+     * @return String representation of the Message object.
+     */
+    public String serialize() {
+        String type = this.type.toString();
+
+        return type + DELIMITER + data + DELIMITER + destinationName + DELIMITER + sourceName;
     }
 
     public String getDestinationName() {
