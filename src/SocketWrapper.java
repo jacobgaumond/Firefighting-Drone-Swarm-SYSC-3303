f02fe6cdb8ee6a3567edab7ff205726c50695438
Jacob Gaumond <jacobgaumond@hotmail.com>
2026-03-15 06:30:27 -0400

    Fix/Change: SocketWrapper.receiveUDPPacket() now correctly catches SocketException on socket close & now returns a success/failure boolean (and also has a new javadoc comment)

--- src/SocketWrapper.java
+++ src/SocketWrapper.java
@@ -18,19 +18,29 @@ public class SocketWrapper {
         socket.close();
     }
 
-    public void receiveUDPPacket(DatagramPacket receivePacket, String subsystemName, String sourceSubsystemName) {
+    /**
+     * Receives a UDP datagram from the socket, returning a boolean signifying method success.
+     *
+     * @param receivePacket         The DatagramPacket to receive on.
+     * @param subsystemName         The name of the subsystem who owns the socket.
+     * @param sourceSubsystemName   The name of the subsystem that sent the DatagramPacket.
+     * @return                      boolean signifying whether the method proceeded successfully/unsuccessfully
+     *                              (i.e., false if the socket is closed while this method is receiving).
+     */
+    public boolean receiveUDPPacket(DatagramPacket receivePacket, String subsystemName, String sourceSubsystemName) {
         try {
             System.out.println("UDP -- " + subsystemName + " WAITING ON (RECEIVING FROM) " + sourceSubsystemName);
             socket.receive(receivePacket); // wait
+        } catch (SocketException se) {
+            System.out.println("UDP -- " + subsystemName + " CANCELED WAITING ON " + sourceSubsystemName + " (SOCKET HAS BEEN CLOSED)");
+            return false;
         } catch (IOException ioe) {
             ioe.printStackTrace();
             System.exit(1);
-        } catch (SocketException se) {
-            System.out.println("UDP -- " + subsystemName + " CANCELED WAITING/RECEIVING (SOCKET HAS BEEN CLOSED)");
-            return;
         }
 
         System.out.println("UDP -- " + subsystemName + " RECEIVED FROM " + sourceSubsystemName + ": " + new String(receivePacket.getData(), 0, receivePacket.getLength()) + "\n");
+        return true;
     }
 
 
