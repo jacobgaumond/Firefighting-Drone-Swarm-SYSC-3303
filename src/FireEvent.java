1c3496c5e9f71aff21e7f20ba239d3b2291267da
Jacob Gaumond <jacobgaumond@hotmail.com>
2026-04-08 19:40:50 -0400

    Refactor: Move getter/setter functions

--- src/FireEvent.java
+++ src/FireEvent.java
@@ -76,15 +76,16 @@ public class FireEvent extends SimulationEvent {
         return faultType;
     }
 
+    public void setFaultType(String s){
+        this.faultType = s;
+    }
+
     public long getTimeInSeconds() {
         String[] parts = time.split(":");
         int hours = Integer.parseInt(parts[0]);
         int minutes = Integer.parseInt(parts[1]);
         int seconds = Integer.parseInt(parts[2]);
         return (hours * 3600) + (minutes * 60) + seconds;
-    }
-    public void setFaultType(String s){
-        this.faultType = s;
     }
 
     public static FireEvent parseFromCsv(String line) {
